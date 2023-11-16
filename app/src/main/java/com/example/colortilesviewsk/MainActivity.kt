afa63edea89a4d68e0f30a8b3c576697710b3f6d
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-11-16 21:45:20 +0800

    feat: Added material colors

--- app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
+++ app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
@@ -1,37 +1,53 @@
 package com.example.colortilesviewsk
 
-import android.R.attr.x
-import android.R.attr.y
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.colortilesviewsk.databinding.ActivityMainBinding
+import kotlin.random.Random
 
-
-// тип для координат
 data class Coord(val x: Int, val y: Int)
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tiles: Array<Array<View>>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tiles: Array<Array<View>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        tiles[0][0] = findViewById(R.id.t00) // TODO: заполнить массив для всех тайлов
-        // еще 15 строк тут, id у плиток нужно по образцу внести в разметку
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tiles = Array(4) { x ->
+            Array(4) { y ->
+                binding.root.findViewWithTag<View>("$x$y")
+            }
+        }
+
+        for (row in tiles) {
+            for (view in row) {
+                val randomColor = if (Random.nextBoolean()) R.color.bright
+                else R.color.dark
+                view.setBackgroundColor(resources.getColor(randomColor))
+            }
+        }
     }
 
     fun getCoordFromString(s: String): Coord {
-        // TODO: реализовать функцию, получающую из
-        // строки вида "12" координаты Coord(1,2)
-        // TODO: сообщать в лог координаты
-        return Coord(0,0) // вернуть полученные координаты
+        val x = s[0].toString().toInt()
+        val y = s[1].toString().toInt()
+        return Coord(x, y)
     }
+
     fun changeColor(view: View) {
-        val brightColor = resources.getColor(R.color.bright)
-        val darkColor = resources.getColor(R.color.dark)
+        val brightColor = ContextCompat.getColor(this, R.color.bright)
+        val darkColor = ContextCompat.getColor(this, R.color.dark)
+
         val drawable = view.background as ColorDrawable
-        if (drawable.color ==brightColor ) {
+        if (drawable.color == brightColor) {
             view.setBackgroundColor(darkColor)
         } else {
             view.setBackgroundColor(brightColor)
@@ -39,18 +55,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(v: View) {
-        val coord = getCoordFromString(v.getTag().toString())
+        val coord = getCoordFromString(v.tag.toString())
         changeColor(v)
 
         for (i in 0..3) {
             changeColor(tiles[coord.x][i])
-            changeColor(tiles[i][coord.x])
+            changeColor(tiles[i][coord.y])
+        }
+
+        if (checkVictory()) {
+            showToast("Победа!")
         }
     }
 
-    fun checkVictory() {
-        // проверка победы
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    private fun checkVictory(): Boolean {
+        val firstColor = (tiles[0][0].background as ColorDrawable).color
+        for (row in tiles) {
+            for (view in row) {
+                val currentColor = (view.background as ColorDrawable).color
+                if (currentColor != firstColor) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
 }
